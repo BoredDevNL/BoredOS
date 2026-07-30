3d620a0a3dd1a2877f943396c39378189b013de2
boreddevnl <chris@boreddev.nl>
2026-07-30 17:59:52 +0200

    fix: initialize FPU and SSE on bootstrap CPU 0
    Signed-off-by: boreddevnl <chris@boreddev.nl>

--- sys/smp.c
+++ sys/smp.c
@@ -122,6 +122,20 @@ static void ap_entry(struct limine_smp_info *info) {
 }
 
 void smp_init_bsp(void) {
+    uint64_t cr0;
+    asm volatile("mov %%cr0, %0" : "=r"(cr0));
+    cr0 &= ~(1ULL << 2);
+    cr0 |= (1ULL << 1);
+    cr0 |= (1ULL << 5);
+    asm volatile("mov %0, %%cr0" : : "r"(cr0));
+
+    uint64_t cr4;
+    asm volatile("mov %%cr4, %0" : "=r"(cr4));
+    cr4 |= (1ULL << 9);
+    cr4 |= (1ULL << 10);
+    asm volatile("mov %0, %%cr4" : : "r"(cr4));
+    asm volatile("fninit");
+
     static cpu_state_t bsp_state_static __attribute__((aligned(64))) = {0};
     bsp_state_static.cpu_id = 0;
     bsp_lapic_id = read_lapic_id();

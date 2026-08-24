83e355fe528f84a2fd0ed21f53941e0bda1d4262
boreddevnl <chris@boreddev.nl>
2026-08-24 14:38:13 +0200

    tests(vmm): create vmm unit tests
    Signed-off-by: boreddevnl <chris@boreddev.nl>

---
+++ mem/test/vmm_test.h
@@ -0,0 +1,12 @@
+// Copyright (c) 2023-2026 Christiaan (chris@boreddev.nl)
+// This software is released under the GNU General Public License v3.0. See LICENSE file for details.
+// This header needs to maintain in any file it is present in, as per the GPL license terms.
+
+#ifndef BOREDOS_VMM_TEST_H
+#define BOREDOS_VMM_TEST_H
+
+#include <stdbool.h>
+
+bool vmm_run_tests(void);
+
+#endif // BOREDOS_VMM_TEST_H

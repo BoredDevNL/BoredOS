e22e2c7c90e82cd2c96fe6464623e25507d5e0ce
boreddevnl <chris@boreddev.nl>
2026-08-24 14:39:17 +0200

    feat(fs): add dirty page flusher
    Signed-off-by: boreddevnl <chris@boreddev.nl>

---
+++ fs/flusher.h
@@ -0,0 +1,11 @@
+// Copyright (c) 2023-2026 Christiaan (chris@boreddev.nl)
+// This software is released under the GNU General Public License v3.0. See LICENSE file for details.
+// This header needs to maintain in any file it is present in, as per the GPL license terms.
+
+#ifndef BOREDOS_FLUSHER_H
+#define BOREDOS_FLUSHER_H
+
+void flusher_init(void);
+void flusher_wake(void);
+
+#endif // BOREDOS_FLUSHER_H

0e2c0dcfe901fc2f31eb1ed577fd8557d111a4c7
boreddevnl <chris@boreddev.nl>
2026-08-24 14:38:59 +0200

    tests(pagecache): create page cache unit tests
    Signed-off-by: boreddevnl <chris@boreddev.nl>

---
+++ mem/test/pagecache_test.h
@@ -0,0 +1,13 @@
+// Copyright (c) 2023-2026 Christiaan (chris@boreddev.nl)
+// This software is released under the GNU General Public License v3.0. See LICENSE file for details.
+// This header needs to maintain in any file it is present in, as per the GPL license terms.
+
+#ifndef BOREDOS_PAGECACHE_TEST_H
+#define BOREDOS_PAGECACHE_TEST_H
+
+#include <stdbool.h>
+
+void test_pagecache_all(void);
+bool pagecache_run_tests(void);
+
+#endif // BOREDOS_PAGECACHE_TEST_H
